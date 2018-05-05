1373618f773f44f97516da34425ff6a0eb55c009
Omnia <omnia.ali36036@yahoo.com>
2018-05-05 07:37:23 +0200

    v1

---
+++ Android/SmartDoorbell/app/src/main/java/com/example/omnia/smartdoorbell/ReplayActivity.java
@@ -0,0 +1,92 @@
+package com.example.omnia.smartdoorbell;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.omnia.smartdoorbell.trusted.InsertTrustedActivity;
+import com.example.omnia.smartdoorbell.trusted.ShowTrustedActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ReplayActivity extends AppCompatActivity {
+
+    String  ip = "192.168.4.1";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_replay);
+
+
+    }
+
+
+
+    public void unlock(View view) {
+        String url="http://" + ip + "/door/open".trim();
+        RequestQueue queue = Volley.newRequestQueue(getBaseContext());
+        // Start the queue
+        queue.start();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+//                JSONObject object = new JSONObject();
+//                    if (object.getString("state").equals("ok")) {
+                Toast.makeText(ReplayActivity.this, "open", Toast.LENGTH_LONG).show();
+//                        Intent intent=new Intent(getBaseContext(),ShowTrustedActivity.class);
+//                        intent.putExtra("ip",ip);
+//                        startActivity(intent);
+//                    }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        // Adding request to request queue
+        queue.add(stringRequest);
+
+
+    }
+
+    public void lock(View view) {
+        String url="http://" + ip + "/door/close".trim();
+        RequestQueue queue = Volley.newRequestQueue(getBaseContext());
+        // Start the queue
+        queue.start();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+//                JSONObject object = new JSONObject();
+//                    if (object.getString("state").equals("ok")) {
+                Toast.makeText(ReplayActivity.this, "close", Toast.LENGTH_LONG).show();
+//                        Intent intent=new Intent(getBaseContext(),ShowTrustedActivity.class);
+//                        intent.putExtra("ip",ip);
+//                        startActivity(intent);
+//                    }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        // Adding request to request queue
+        queue.add(stringRequest);
+    }
+}
